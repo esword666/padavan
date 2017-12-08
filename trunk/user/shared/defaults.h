39cdb7b700efb0f8b645b69c20817342f473e67a
Eth <esword666@163.com>
2017-12-08 10:40:58 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -28,13 +28,13 @@
 #define SYS_USER_NOBODY		"nobody"
 #define SYS_GROUP_NOGROUP	"nogroup"
 
-#define DEF_LAN_ADDR		"192.168.1.21"
+#define DEF_LAN_ADDR		"192.168.1.1"
 #define DEF_LAN_DHCP_BEG	"192.168.1.200"
 #define DEF_LAN_DHCP_END	"192.168.1.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"US"
+#define DEF_WLAN_5G_CC		"CN"
 #define DEF_WLAN_2G_SSID	"Hsu"
 #define DEF_WLAN_5G_SSID	"Hsu_5G"
 #define DEF_WLAN_2G_GSSID	"Hsu_GUEST"
